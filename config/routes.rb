fda34b84feee0433708ce41f02f4b50e2cce74f6
Roger Vera <rovera@vassar.edu>
2018-12-19 17:54:48 -0500

    login route and action handled

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       post '/search', action: :search, controller: 'lyrics_search_engine'
       get '/login', action: :login, controller: 'login'
+      get '/login/callback', action: :callback, controller: 'login'
     end
   end
 end
