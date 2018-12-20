472a11b725146fea0456bafc788cae0ac7b9f1bf
Roger Vera <rovera@vassar.edu>
2018-12-20 10:37:59 -0500

    error handling if user declines authorization

--- app/controllers/api/v1/spotify_users_controller.rb
+++ app/controllers/api/v1/spotify_users_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::SpotifyUsersController < ApplicationController
 
   def create
-    byebug
+    # handle response from Spotify
+    if params[:error]
+      redirect_to ENV['LISTEN_BY_LYRICS_HOME_URL']
+    end
   end
 
 end
