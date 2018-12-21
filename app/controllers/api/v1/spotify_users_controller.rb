f821161317a66d5791bb73d75c10d49f3c363970
Roger Vera <rovera@vassar.edu>
2018-12-21 10:59:19 -0500

    update SpotifyUser controlelr

--- app/controllers/api/v1/spotify_users_controller.rb
+++ app/controllers/api/v1/spotify_users_controller.rb
@@ -1,9 +1,39 @@
 class Api::V1::SpotifyUsersController < ApplicationController
 
   def create
-    # handle response from Spotify
+    # Handle response from Spotify
     if params[:error]
       redirect_to ENV['LISTEN_BY_LYRICS_HOME_URL']
+    else
+      # Assemble and send request to Spotify for access and refresh token
+      body = {
+        grant_type: "authorization_code",
+        code: params[:code],
+        redirect_uri: ENV['REDIRECT_URI'],
+        client_id: ENV['SPOTIFY_CLIENT_ID'],
+        client_secret: ENV['SPOTIFY_CLIENT_SECRET']
+      }
+
+      auth_response = RestClient.post('https://accounts.spotify.com/api/token', body)
+      # convert response.body to json for assisgnment
+      auth_params = JSON.parse(auth_response.body)
+      # assemble and send request to Spotify for user profile information
+      header = {
+        Authorization: "Bearer #{auth_params["access_token"]}"
+      }
+      user_response = RestClient.get("https://api.spotify.com/v1/me", header)
+      # convert response.body to json for assisgnment
+      user_params = JSON.parse(user_response.body)
+
+      # create new user based on response, or find the existing user in database
+      @spotify_user = SpotifyUser.find_or_create_by(spotify_id: user_params['id'],
+                      display_name: user_params['display_name'],
+                      url: user_params['external_urls']['spotify'],
+                      href: user_params['href'],
+                      uri: user_params['uri'])
+      img_url = user_params["images"][0] ? user_params["images"][0]["url"] : nil
+      @spotify_user.update(profile_image: img_url)
+      @spotify_user.update(access_token: auth_params["access_token"], refresh_token:  auth_params["refresh_token"])
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_20_145108) do
+ActiveRecord::Schema.define(version: 2018_12_20_212635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2018_12_20_145108) do
     t.string "profile_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "access_token"
+    t.string "refresh_token"
   end
 
 end
