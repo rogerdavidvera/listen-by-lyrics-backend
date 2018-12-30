a5795010639163c04265109950ab16ab2842cc43
Roger Vera <rovera@vassar.edu>
2018-12-30 13:01:24 -0500

    play request working

--- app/controllers/api/v1/playback_controller.rb
+++ app/controllers/api/v1/playback_controller.rb
@@ -1,5 +1,27 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 class Api::V1::PlaybackController < ApplicationController
   def play
-    track_uri = params[:track_uri]
+    track_id = params[:track_uri]
+    # current_user.refresh_access_token
+    send_play_post_request(track_id)
   end
+
+  private
+
+  def send_play_post_request(track_id)
+    uri = URI.parse('https://api.spotify.com/v1/me/player/play')
+    request = Net::HTTP::Put.new(uri)
+    request.content_type = 'application/json'
+    request["Accept"] = "application/json"
+    request["Authorization"] = "Bearer #{current_user.access_token}"
+    request.body = JSON.dump({"uris" => ["spotify:track:#{track_id}"]})
+    req_options = {use_ssl: uri.scheme == "https"}
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+  end
+
 end
