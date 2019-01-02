f439d23ba2a61f517fa210ffc11723bc972f708f
Roger Vera <rovera@vassar.edu>
2019-01-02 16:55:04 -0500

    add spotify_track_id to track model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_02_214145) do
+ActiveRecord::Schema.define(version: 2019_01_02_215409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2019_01_02_214145) do
     t.integer "spotify_user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "spotify_track_id"
   end
 
 end
